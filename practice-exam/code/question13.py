d5e883c8f2392c4d8f62152823d6745f53677c8a
Dev Mody <devmody@devs-mbp-2.home>
2024-12-10 00:50:08 -0500

    asdjllasjd

--- practice-exam/code/question13.py
+++ practice-exam/code/question13.py
@@ -4,7 +4,3 @@ for x, s in pairs:
     if pow(s, e, n) == x:
         print((x, s), "Valid")
     
-    
-# Element a in Zp
-# if a^k = len(Zp) then a is a generator
-# if it is not, it is not a generator of Zp, but it is a generator in general. If k = 3. Then a is a generator of all subgroups of length 3.
